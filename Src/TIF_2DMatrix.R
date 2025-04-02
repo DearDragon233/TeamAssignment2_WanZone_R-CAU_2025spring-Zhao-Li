6b160a18346edf7381c8b60eb07bc5b3797c7dba
Yucheng Peng <pengyucheng@cau.edu.cn>
2025-04-02 21:34:45 +0800

    Create TIF_2DMatrix.R
    Create TIF_2DMatrix.R

---
+++ Src/TIF_2DMatrix.R
@@ -0,0 +1,24 @@
+library(reshape2)  # 加载 reshape2
+library(terra)     # 加载 terra 处理栅格数据
+
+# 读取 .tif 并降低分辨率
+r <- rast("Data/Raw/glc2000_v1_1.tif")
+r <- aggregate(r, fact=2)  # 2 倍降采样，减少数据量
+
+# 转换为数据框
+df <- as.data.frame(r, xy = TRUE)
+
+# 加载 RData
+load("Data/Processed/TIFtransfertoRData.RData")
+
+# 确保列名正确
+names(df) <- c("x", "y", "value")
+
+# 转换为二维矩阵 (y 为行，x 为列)
+mat <- acast(df, y ~ x, value.var = "value")
+
+# 查看矩阵的维度
+dim(mat)
+
+# 保存为 RData
+save(mat, file = "Data/Processed/TIF_2DMatrix.RData")
