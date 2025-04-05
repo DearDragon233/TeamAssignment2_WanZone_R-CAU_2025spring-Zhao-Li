d59d087b05fed65dfc2936d44b1be40e83a2a601
皮始皇 <2024333020402@cau.edu.cn>
2025-04-05 18:35:58 +0800

    Melt TIFtranfertoXLSX.R and TIF_2DMatrix.R
    Avoid unnecessarily large .xlsx file output

--- Src/Processing/TIF_2DMatrix.R
+++ Src/Processing/TIF_2DMatrix.R
@@ -1,15 +1,15 @@
-library(reshape2)  # 加载 reshape2
-library(terra)     # 加载 terra 处理栅格数据
+#加载R包
+library(reshape2)
+library(terra)     # 用于处理栅格数据
 
 # 读取 .tif 并降低分辨率
 r <- rast("Data/Raw/glc2000_v1_1.tif")
-r <- aggregate(r, fact=2)  # 2 倍降采样，减少数据量
+
+# 2 倍降采样，减少数据量
+r <- aggregate(r, fact=2)
 
 # 转换为数据框
 df <- as.data.frame(r, xy = TRUE)
-
-# 加载 RData
-load("Data/Processed/TIFtransfertoRData.RData")
 
 # 确保列名正确
 names(df) <- c("x", "y", "value")
@@ -20,5 +20,5 @@ mat <- acast(df, y ~ x, value.var = "value")
 # 查看矩阵的维度
 dim(mat)
 
-# 保存为 RData
+# 保存为.RData文件
 save(mat, file = "Data/Processed/TIF_2DMatrix.RData")
