4fd1435a6e32650463b8990eff65e781f37c15f2
皮始皇 <2024333020402@cau.edu.cn>
2025-04-06 16:12:10 +0800

    Create Vegetation type - latitude density map
    This image is used to describe the proportion of the total land area of each vegetation type at different latitudes

---
+++ Src/Drawing/Vegetation type - latitude density map.R
@@ -0,0 +1,27 @@
+# 加载R包
+library(reshape2) # 用于转换为长格式
+library(ggplot2)  # 伟大，无需多言（
+
+# 加载数据
+load("Data/Processed/land_fraction_df.RData") # 植被分布信息
+load("Data/Processed/plant_colors.RData")     # 图例颜色信息
+
+# 数据框长格式转换
+land_fraction_long <- melt(land_fraction_df, id.vars = "Latitude",
+                           variable.name = "Vegetation", value.name = "Fraction")
+
+# ggplot，启动！
+ggplot(land_fraction_long, aes(x = Latitude, y = Fraction, fill = Vegetation)) +
+  geom_density(stat = "identity", position = "stack",color = NA) +  # 颜色NA为无边框，stack为堆叠图
+  scale_fill_manual(values = plant_colors) +
+  theme_minimal() +
+  scale_x_continuous(expand = c(0, 0)) +  # 移除 x 轴额外留白
+  labs(title = "植被类型占陆地比例-纬度密度图",
+       x = "纬度/°",
+       y = "植被类型占陆地比例",
+       fill = "植被类型")+
+  theme(legend.position = "none")+  # 移除图例，便于拼接
+  coord_flip()                      # 旋转图像匹配地图方向
+
+# 存储图像
+ggsave("Plots/植被类型占陆地比例-纬度密度图.png",width = 8, height = 10)
