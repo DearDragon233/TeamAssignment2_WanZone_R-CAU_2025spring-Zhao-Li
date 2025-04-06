20f5f02ec7fb6481ee4e82dd5990637fcf1fbdbe
皮始皇 <2024333020402@cau.edu.cn>
2025-04-06 15:46:25 +0800

    Create Color_conversion.R and output file
    This script is used to convert RGB color information to hexadecimal color information

---
+++ Src/Processing/Color_conversion.R
@@ -0,0 +1,13 @@
+# 加载R包
+library(readxl)
+library(scales)   # 用于颜色转换
+
+# 加载图例文件
+leg <- read_excel("Data/Raw/Global_Legend.xls")
+
+# 将图例RGB信息转换为16进制颜色代码
+plant_colors <- rgb(leg[[3]], leg[[4]], leg[[5]]) # 3,4,5 列对应 R,G,B值
+names(plant_colors) <- leg$CLASSNAMES             # 将颜色名称映射到植被类别
+
+# 保存为.RData文件
+save(plant_colors, file = "Data/Processed/plant_colors.RData")  # 图例颜色
