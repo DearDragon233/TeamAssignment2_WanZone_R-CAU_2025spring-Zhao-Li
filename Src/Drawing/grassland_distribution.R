892946d3b0b4c2de3125fde9bd304b49b8baadcb
Yucheng Peng <pengyucheng@cau.edu.cn>
2025-04-06 18:43:43 +0800

    Pushing grassland related (still no results)
    Pushing grassland related (still no results)

--- Src/Drawing/grassland_distribution.R
+++ Src/Drawing/grassland_distribution.R
@@ -7,8 +7,12 @@ load("Data/Processed/TIF_2DMatrix.RData")
 # 找出草地对应的 VALUE
 grassland_values <- c(13,14,15)
 
+# 数据采样：每隔一定间隔选取数据
+sampling_interval <- 5  # 可以根据实际情况调整
+mat_sampled <- mat[seq(1, nrow(mat), by = sampling_interval), seq(1, ncol(mat), by = sampling_interval)]
+
 # 匹配矩阵中的草地值
-is_grassland <- mat %in% grassland_values
+is_grassland <- mat_sampled %in% grassland_values
 
 # 确保 is_grassland 是矩阵
 if (!is.matrix(is_grassland)) {
@@ -26,6 +30,8 @@ print(dim(melted_mat))
 ggplot(melted_mat, aes(x = longitude, y = latitude, fill = is_grassland)) +
   geom_raster() +
   scale_fill_manual(values = c("FALSE" = "white", "TRUE" = "green"), name = "是否为草地") +
+  scale_x_continuous(limits = c(-180.0, 180.0)) +
+  scale_y_continuous(limits = c(-56.0, 89.9)) +  # 设置正确的纬度范围
   labs(x = "经度", y = "纬度", title = "草地分布热力图") +
   theme_minimal()
 
