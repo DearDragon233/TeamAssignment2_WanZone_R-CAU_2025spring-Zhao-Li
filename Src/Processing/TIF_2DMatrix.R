0581d9cbdfd191ec810ec1ca74ee20909a597e24
皮始皇 <2024333020402@cau.edu.cn>
2025-04-06 12:55:30 +0800

    Modify coordinate system
    Cancel pixel area mapping and add table header assigned by latitude and longitude to the output

--- Src/Processing/TIF_2DMatrix.R
+++ Src/Processing/TIF_2DMatrix.R
@@ -11,8 +11,6 @@ crs(r) <- "EPSG:4326"
 # 手动指定经纬度范围与像素对应信息（来自文件"glc2000_v1_1_projinfo.hdr"）
 # 经纬度范围
 ext(r) <- ext(-180.000000, 179.991070, -56.008928, 89.991071)
-# 像素对应范围
-res(r) <- c(0.0089285714, 0.0089285714)
 
 # 2 倍降采样，减少数据量
 r <- aggregate(r, fact=2)
@@ -25,6 +23,14 @@ mat <- round(mat)
 
 # 查看矩阵的维度
 dim(mat)
+
+# 计算每行中心经度、纬度
+longitudes <- terra::xFromCol(r, 1:ncol(r))
+latitudes <- terra::yFromRow(r, 1:nrow(r))
+
+# 将经纬度作为矩阵行列名
+colnames(mat) <- longitudes
+rownames(mat) <- latitudes
 
 # 保存为.RData文件
 save(mat, file = "Data/Processed/TIF_2DMatrix.RData")
